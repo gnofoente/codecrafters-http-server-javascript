3977ecf11eb716ffbc6bde213bb6a6acb66f5bf7
Gustavo Nofoente <gustavonofoente@gmail.com>
2024-04-01 21:01:31 -0300

    Stage 3 - Listen and respond with different statuses

--- app/main.js
+++ app/main.js
@@ -1,10 +1,30 @@
 const net = require("net");
+const CRLF = '\r\n\r\n';
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  socket.on('data', (data) => {
+    const dataStr = data.toString('utf-8');
+    const dataStrLines = dataStr.split('\r\n');
+    const requestStr = dataStrLines[0];
+    const [verb, path, version] = requestStr.split(' ');
+
+    let status;
+
+    if (path === '/') {
+      status = `200 OK`;
+    } else {
+      status = `404 NOT FOUND`;
+    }
+    setTimeout(() => {
+      socket.write(`HTTP/1.1 ${status} ${CRLF}`);
+      socket.end();
+    }, 1000);
+  });
+
   socket.on("close", () => {
     console.log("socket closed");
     socket.end();
@@ -12,8 +32,3 @@ const server = net.createServer((socket) => {
 });
 
 server.listen(4221, "localhost");
-
-server.on("connection", (socket) => {
-  socket.write('HTTP/1.1 200 OK\r\n\r\n');
-  socket.end();
-});
