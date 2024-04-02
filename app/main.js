ab5f7497111f7965b6f06190c125a707bfffb588
Gustavo Nofoente <gustavonofoente@gmail.com>
2024-04-01 22:04:50 -0300

    Stage 4

--- app/main.js
+++ app/main.js
@@ -4,29 +4,73 @@ const CRLF = '\r\n\r\n';
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
+const Request = function (data) {
+  const dataStr = data.toString('utf-8');
+  const dataStrLines = dataStr.split('\r\n');
+  const requestStr = dataStrLines.shift();
+  const [verb, path, version] = requestStr.split(' ');
+  const headers = {};
+
+  dataStrLines.forEach((line) => {
+    const [key, value] = line.split(/:(.*)/s);
+    if (!key) return;
+    headers[key] = value.trim();
+  });
+
+  return {
+    verb,
+    path,
+    version,
+    headers
+  };
+}
+
+const Response = function (statusCode, headers, body) {
+  const status = {
+    '200': '200 OK',
+    '404': '404 NOT FOUND'
+  };
+
+  function getHeadersAsString() {
+    let str = '';
+    for (key in headers) {
+      const value = headers[key];
+      str += `${key}: ${value}\r\n`;
+    }
+    return str;
+  }
+
+  function toString() {
+    const code = typeof statusCode !== 'string' && statusCode.toString();
+    const headers = getHeadersAsString();
+    return `HTTP/1.1 ${status[code]}\r\n${headers}\r\n${body}`
+  }
+
+  return {
+    toString
+  };
+}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
   socket.on('data', (data) => {
-    const dataStr = data.toString('utf-8');
-    const dataStrLines = dataStr.split('\r\n');
-    const requestStr = dataStrLines[0];
-    const [verb, path, version] = requestStr.split(' ');
+    const { path } = new Request(data);
 
-    let status;
-
-    if (path === '/') {
-      status = `200 OK`;
-    } else {
-      status = `404 NOT FOUND`;
+    const [none, randomStr] = path.split('/echo/');
+    if (randomStr) {
+      const headers = {
+        'Content-Type': 'text/plain',
+        'Content-Length': randomStr.length,
+      };
+      const res = new Response(200, headers, randomStr);
+      socket.write(res.toString());
     }
-    setTimeout(() => {
-      socket.write(`HTTP/1.1 ${status} ${CRLF}`);
-      socket.end();
-    }, 1000);
+
+    socket.end();
   });
 
   socket.on("close", () => {
-    console.log("socket closed");
+    console.log("* socket closed");
     socket.end();
   });
 });
