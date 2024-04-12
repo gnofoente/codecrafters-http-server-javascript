5b71e05c1af87bb40eb7ed0c2087fb1df3930d91
Gustavo Nofoente <gustavonofoente@gmail.com>
2024-04-11 22:18:30 -0300

    Finished

--- app/main.js
+++ app/main.js
@@ -1,18 +1,21 @@
 const net = require("net");
 const process = require("node:process");
-const { readFile } = require("node:fs/promises");
+const fs = require("node:fs");
 
 // You can use print statements as follows for debugging, they'll be visible when running tests.
 console.log("Logs from your program will appear here!");
 
 const Request = function (data) {
   const dataStr = data.toString('utf-8');
-  const dataStrLines = dataStr.split('\r\n');
-  const requestStr = dataStrLines.shift();
+  const [main, body] = dataStr.split('\r\n\r\n');
+  const lines = main.split('\r\n');
+  
+  const requestStr = lines.shift();
   const [verb, path, version] = requestStr.split(' ');
+
   const headers = {};
 
-  dataStrLines.forEach((line) => {
+  lines.forEach((line) => {
     const [key, value] = line.split(/:(.*)/s);
     if (!key) return;
     headers[key] = value.trim();
@@ -22,6 +25,7 @@ const Request = function (data) {
   this.path = path;
   this.version = version;
   this.headers = headers;
+  this.body = body;
 
   return this;
 }
@@ -37,7 +41,8 @@ const Response = function (statusCode, headers = {}, body = '') {
 
   const status = {
     '200': '200 OK',
-    '404': '404 NOT FOUND'
+    '404': '404 NOT FOUND',
+    '201': '201 CREATED'
   };
 
   function getHeadersAsString() {
@@ -60,36 +65,79 @@ const Response = function (statusCode, headers = {}, body = '') {
   };
 }
 
+const Router = function () {
+  const registeredRequests = new Map();
+
+  this.register = ([verb, path], callback) => {
+    const formattedPath = path
+      .replace('*', '(.*)');
+    const regExpStr = `^${verb}__${formattedPath}$`;
+    const key = new RegExp(regExpStr);
+    registeredRequests.set(key, callback);
+  }
+
+  this.handle = (request) => {
+    const verbAndPath = `${request.verb}__${request.path}`;
+    let output;
+
+    registeredRequests.forEach((callback, key) => {
+      if (key.test(verbAndPath)) {
+        output = callback(request);
+        return;
+      }
+    });
+
+    return output || new Response(404);
+  }
+
+  this.getRegisteredRequests = () => registeredRequests;
+}
+
 // Uncomment this to pass the first stage
 const server = net.createServer((socket) => {
+  const router = new Router();
+
+  router.register(['GET', '/'], () => {
+    return new Response(200);
+  });
+
+  router.register(['GET', '/echo/*'], (req) => {
+    const [none, echo] = req.path.split('/echo/');
+    return new Response(200, {}, echo);
+  });
+
+  router.register(['GET', '/user-agent'], (req) => {
+    const userAgent = req.headers['User-Agent'];
+    return new Response(200, {}, userAgent);
+  });
+
+  router.register(['GET', '/files/*'], (req) => {
+    const [none, fileName] = req.path.split('/files/');
+    const indexOfDirectory = process.argv.indexOf('--directory');
+    const dir = process.argv[indexOfDirectory + 1];
+
+    let res;
+    try {
+      const contents = fs.readFileSync(dir + fileName, { encoding: 'utf-8' });
+      res = new Response(200, { 'Content-Type': 'application/octet-stream' }, contents);
+    } catch {
+      res = new Response(404);
+    } finally {
+      return res;
+    }
+  });
+
+  router.register(['POST', '/files/*'], (req) => {
+    const [none, fileName] = req.path.split('/files/');
+    const indexOfDirectory = process.argv.indexOf('--directory');
+    const dir = process.argv[indexOfDirectory + 1];
+    fs.writeFileSync(dir + fileName, req.body);
+    return new Response(201);
+  });
+
   socket.on('data', async (data) => {
     const req = new Request(data);
-    let res;
-
-    if (req.path === '/') {
-      res = new Response(200);
-    } else if (req.path.match(/\/echo\/(.*)/)) {
-      const [none, echo] = req.path.split('/echo/');
-      res = new Response(200, {}, echo);
-    } else if (req.path === '/user-agent') {
-      const userAgent = req.headers['User-Agent'];
-      res = new Response(200, {}, userAgent);
-    } else if (req.path.match(/\/files\/(.*)/)) {
-      console.log('files');
-      const [none, fileName] = req.path.split('/files/');
-      const indexOfDirectory = process.argv.indexOf('--directory');
-      const dir = process.argv[indexOfDirectory + 1];
-      try {
-        const contents = await readFile(dir + fileName, { encoding: 'utf-8' });
-        console.log(contents);
-        res = new Response(200, { 'Content-Type': 'application/octet-stream' }, contents);
-      } catch {
-        res = new Response(404);
-      }
-    } else {
-      res = new Response(404);
-    }
-
+    const res = router.handle(req);
     socket.write(res.toString());
     socket.end();
   });
